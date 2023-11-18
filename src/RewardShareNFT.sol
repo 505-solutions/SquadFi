304069149bce7430adf0af68c9536b9b6a736439
Snojj25 <snoj.jure1@gmail.com>
2023-11-18 21:45:34 +0100

    usi testi delajo kar je večja sreča kot pa luka ki je zmagu preprogo

--- src/RewardShareNFT.sol
+++ src/RewardShareNFT.sol
@@ -64,23 +64,39 @@ contract RewardShareNFT is ERC1155URIStorage, Ownable {
             if (feeRecipient == address(0)) continue;
 
             uint256 id = feePercentages[i] / spliterScaleFactor;
-            console.log(feeRecipient, id);
 
             _mint(feeRecipient, id, 1, bytes(""));
             addToStorageArray(s_userFeeShares[feeRecipient][id], validatorId);
         }
 
-        // balanceOf(address account, uint256 id)
+        // uint256 bal = balanceOf(feeRecipients[0], 25);
 
-        // s_validatorSplitFeeAddresses[validatorId] = feeRecipients;
-        // s_validatorSplitFeePercentages[validatorId] = feePercentages;
+        s_validatorSplitFeeAddresses[validatorId] = feeRecipients;
+        s_validatorSplitFeePercentages[validatorId] = feePercentages;
+        newSpliterAddress = address(123456789);
         // newSpliterAddress = ISplitMain(spliterAddress).createSplit(
         //     feeRecipients,
         //     feePercentages,
         //     0,
         //     address(this)
         // );
-        // s_feeSplitter[validatorId] = newSpliterAddress;
+        s_feeSplitter[validatorId] = newSpliterAddress;
+
+        // console.log("newSpliterAddress: %s", s_feeSplitter[validatorId]);
+        // console.log(
+        //     "s_validatorSplitFeeAddresses: %s",
+        //     s_validatorSplitFeeAddresses[validatorId][0],
+        //     s_validatorSplitFeeAddresses[validatorId][3]
+        // );
+        // console.log(
+        //     "s_validatorSplitFeePercentages: %s",
+        //     s_validatorSplitFeePercentages[validatorId][0],
+        //     s_validatorSplitFeePercentages[validatorId][0]
+        // );
+        // console.log(
+        //     "newSpliterAddress: %s",
+        //     s_userFeeShares[feeRecipients[0]][25][0]
+        // );
     }
 
     function safeTransferFrom(
@@ -175,12 +191,12 @@ contract RewardShareNFT is ERC1155URIStorage, Ownable {
         removeToStorageArray(s_userFeeShares[from][id], validatorId);
         addToStorageArray(s_userFeeShares[to][id], validatorId);
 
-        ISplitMain(spliterAddress).updateSplit(
-            validatorSplitAddr,
-            feeAddresses,
-            feePercentages,
-            0
-        );
+        // ISplitMain(spliterAddress).updateSplit(
+        //     validatorSplitAddr,
+        //     feeAddresses,
+        //     feePercentages,
+        //     0
+        // );
     }
 
     function splitFeeDenominations(
@@ -231,7 +247,8 @@ contract RewardShareNFT is ERC1155URIStorage, Ownable {
         uint256 element
     ) private {
         if (storage_array[storage_array.length - 1] == element) {
-            storage_array.pop();
+            delete storage_array[storage_array.length - 1];
+
             return;
         }
 
